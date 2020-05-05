d442e0375956b4401b23c9e2450ec30aebd68742
anusha216 <k.anusha@suntechnoservices.com>
2020-05-05 12:59:32 +0530

    new Commit

--- src/main/java/com/vedika/functionhall/controller/OwnerController.java
+++ src/main/java/com/vedika/functionhall/controller/OwnerController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,11 @@ import com.vedika.functionhall.model.Owner;
 import com.vedika.functionhall.model.Response;
 import com.vedika.functionhall.service.OwnerService;
 
-//Required imports
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class OwnerController {
+	
 
 	@Autowired
 	private OwnerService ownerService;
